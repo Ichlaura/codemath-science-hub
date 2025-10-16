e1f0cdc0913544b221060193fb611f9bb7f3bd2b
Ichlaura <laupao41@gmail.com>
2025-10-16 16:30:18 +0900

    Update Google OAuth callbackURL

--- config/oauth.js
+++ config/oauth.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+  callbackURL: `${process.env.BASE_URL}/auth/google/callback`,
   passReqToCallback: true
 },
 async function(request, accessToken, refreshToken, profile, done) {
